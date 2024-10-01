aa7dbcfbe8607509a3a8218f802c7a52d22027ed
root <christopherwright435@gmail.com>
2024-10-01 10:15:23 +0000

    style: fix long lines and trailing whitespace in file_storage.py for PEP 8 compliance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,8 +9,7 @@ from models.review import Review
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances"""
-
+    """Serializes instances to a JSON file and deserializes them back"""
     __file_path = "file.json"
     __objects = {}
 
@@ -25,26 +24,17 @@ class FileStorage:
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        with open(self.__file_path, 'w') as f:
-            json.dump({key: obj.to_dict() for key, obj in self.__objects.items()}, f)
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects if it exists"""
+        """Deserializes the JSON file to __objects if the file exists"""
         try:
-            with open(self.__file_path, 'r') as f:
-                objects = json.load(f)
-                for key, value in objects.items():
-                    cls_name = key.split('.')[0]
-                    cls = {
-                        "BaseModel": BaseModel,
-                        "User": User,
-                        "State": State,
-                        "City": City,
-                        "Amenity": Amenity,
-                        "Place": Place,
-                        "Review": Review
-                    }.get(cls_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    class_name = value["__class__"]
+                    self.__objects[key] = eval(class_name)(**value)
         except FileNotFoundError:
-            pass 
+            pass
